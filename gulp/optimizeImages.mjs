3b4b608fa14df0a084aff59ca0e4a4b062a93ec2
Vladimir Trofimov <i_am@palant.org>
2023-04-20 16:47:54 +0300

    Adds header styles

--- gulp/optimizeImages.mjs
+++ gulp/optimizeImages.mjs
@@ -1,8 +1,10 @@
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 import webp from 'gulp-webp';
-import {stacksvg} from 'gulp-stacksvg';
+import pngQuant from 'imagemin-pngquant';
+import mozJpeg from 'imagemin-mozjpeg';
 import svgo from 'imagemin-svgo';
+import {stacksvg} from 'gulp-stacksvg';
 
 const sprite = () =>
   gulp
@@ -49,6 +51,25 @@ const optimizeSvg = () =>
             })]))
       .pipe(gulp.dest('build/img'));
 
+const optimizeJpg = () =>
+  gulp
+      .src('build/img/**/*.{jpg,jpeg}')
+      .pipe(imagemin([mozJpeg({quality: 90, progressive: true})]))
+      .pipe(gulp.dest('build/img'));
+
+const optimizePng = () =>
+  gulp
+      .src('build/img/**/*.png')
+      .pipe(
+          imagemin([
+            pngQuant({
+              speed: 1,
+              strip: true,
+              dithering: 1,
+              quality: [0.8, 0.9],
+            })]))
+      .pipe(gulp.dest('build/img'));
+
 /*
   Optional tasks
   ---------------------------------
@@ -64,8 +85,8 @@ const createWebp = () => {
   const root = '';
   return gulp
       .src(`source/img/${root}**/*.{png,jpg}`)
-      .pipe(webp({quality: 75}))
+      .pipe(webp({quality: 90}))
       .pipe(gulp.dest(`source/img/${root}`));
 };
 
-export {sprite, createWebp, optimizeSvg};
+export {sprite, createWebp, optimizeSvg, optimizePng, optimizeJpg};
