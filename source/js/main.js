cc3b5780b1ce7190190fb0eb40a18868bcb7c358
Vladimir Trofimov <i_am@palant.org>
2023-04-24 15:55:46 +0300

    Adds tabs

--- source/js/main.js
+++ source/js/main.js
@@ -1,44 +1,21 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {findVideos} from './modules/video/video';
+import {TabsAutomatic} from './modules/tabs';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  // Utils
-  // ---------------------------------
-
   iosVhFix();
   findVideos();
 
-  // Modules
-  // ---------------------------------
+});
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
+window.addEventListener('load', function () {
+  const tablists = document.querySelectorAll('[role=tablist]');
+  for (let i = 0; i < tablists.length; i++) {
+    new TabsAutomatic(tablists[i]);
+  }
 });
 
 // ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
