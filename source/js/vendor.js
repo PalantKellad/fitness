455190178272e34fbd6bd8fb02dfbe0f547b22b9
Vladimir Trofimov <i_am@palant.org>
2023-04-27 14:45:52 +0300

    Adds slider

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,18 @@
-import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+import Swiper from './vendor/swiper';
+
+const slider = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  a11y: {
+    enabled: true,
+    prevSlideMessage: 'Предыдущий слайд',
+    nextSlideMessage: 'Следующий слайд',
+  },
+  slidesPerView: 4,
+  spaceBetween: 40,
+});
